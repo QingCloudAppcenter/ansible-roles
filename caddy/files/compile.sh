cc50158ba3c2838cc9e286e6bb6b7dad343839f6
Hongliang Wang <hongliangwang@yunify.com>
2019-12-06 20:20:40 +0800

    update caddy

---
+++ caddy/files/compile.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# From https://github.com/wmark/http.upload/issues/38#issuecomment-529623377
+
+cat > go.mod << MOD_EOF
+module caddy
+
+go 1.13
+
+require (
+  blitznote.com/src/http.upload/v3 v$CADDY_UPLOAD_VERSION
+  github.com/caddyserver/caddy v$CADDY_VERSION
+)
+MOD_EOF
+
+cat > main.go << EOF
+package main
+
+import (
+  "github.com/caddyserver/caddy/caddy/caddymain"
+
+  _ "blitznote.com/src/http.upload/v3"
+)
+
+func main() {
+  caddymain.Run()
+}
+EOF
+
+gofmt -w main.go
+go mod tidy
+go build -tags "caddyserver0.9 caddyserver1.0" -o $TARGET_FILE
